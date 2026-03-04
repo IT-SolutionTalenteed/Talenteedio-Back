0f771062e776a0c43ff523ee166d37dbc0509d90
Happy-Franck <solofonirina35@gmail.com>
2026-03-04 20:32:25 +0300

    lasa

--- start-tsnode.sh
+++ start-tsnode.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 18.17
-
 cd /home/talenteedio/sites/Talenteed-Back
-
-# Lancer le serveur compilé
-node dist/index.js
+npm install        # facultatif si déjà installé
+npx ts-node ./src/index.ts
